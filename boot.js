b177868ce3085964b73e9d0f8bdb1fa6aa09e36f
Lucas Barrena <lucasbarrena98@gmail.com>
2022-11-16 17:27:25 -0300

    boot from drive

--- boot.js
+++ boot.js
@@ -3,13 +3,15 @@ const path = require('path')
 const minimist = require('minimist')
 const ScriptLinker = require('script-linker')
 const builtinModules = new Set(require('module').builtinModules)
+const Hyperdrive = require('hyperdrive')
+const Corestore = require('corestore')
 
 const argv = minimist(process.argv.slice(2))
-if (!argv.entrypoint) throw new Error('--entrypoint is required')
+if (!argv.store) throw new Error('--store <path to corestore> is required')
+if (!argv.entrypoint) throw new Error('--entrypoint <main filename of drive> is required')
 
-const entrypoint = path.resolve(argv.entrypoint)
-// const root = path.dirname(entrypoint)
-// const entrypoint = path.join('/', path.basename(entrypoint))
+const corestore = new Corestore(argv.store)
+const drive = new Hyperdrive(corestore)
 
 if (argv['add-module']) {
   const mods = argv['add-module']
@@ -19,9 +21,11 @@ if (argv['add-module']) {
 
 const s = new ScriptLinker({
   cacheSize: Infinity,
-  readFile (name) {
+  async readFile (name) {
     // console.log('readFile', name)
-    return fs.promises.readFile(name)
+    const buffer = await drive.get(name)
+    if (!buffer) throw new Error('some err like fs.promises.readFile')
+    return buffer
   }
 })
 
@@ -30,7 +34,7 @@ main()
 async function main () {
   let first = null
 
-  for await (const dep of s.dependencies(entrypoint)) {
+  for await (const dep of s.dependencies(argv.entrypoint)) {
     if (!first) first = dep
   }
 
