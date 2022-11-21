64ae8e14fc458b3a9e5905ad7b336c004e301180
Lucas <lucasbarrena98@gmail.com>
2022-11-21 22:44:45 +0800

    First release (#1)
    * mafintosh's version with lukks's sprinkles
    
    * boot from drive
    
    * Create README.md
    
    * feedback
    
    * fix builtinModules
    
    * add error for MODULE_NOT_FOUND
    
    * eslint-disable-line no-new-func
    
    * return run value
    
    * add dev deps
    
    * Create test.js
    
    * dmc feedback
    
    * Create LICENSE
    
    * feedback

---
+++ README.md
@@ -0,0 +1,46 @@
+# boot
+
+Boot a hyperdrive from memory.
+
+```
+npm i holepunchto/boot
+```
+
+## Usage
+```js
+const boot = new Boot(drive)
+await boot.start()
+```
+
+## API
+
+#### `const boot = new Boot(drive, [options])`
+
+Creates a bootloader to run the drive.
+
+Available `options`:
+```js
+{
+  modules: []
+}
+```
+
+`modules` is used to add more native modules.
+
+#### `drive.modules`
+
+List of modules that are included in the boot process.\
+By default it contains all the native modules.
+
+Add more by `drive.modules.add(name)` or using `options` from the constructor.
+
+#### `await drive.start([entrypoint])`
+
+Runs the drive.
+
+If `entrypoint` is not set, then it will try `/package.json` `main` property.
+
+If it fails to find an `entrypoint` then the boot process will fail.
+
+## License
+MIT
---
+++ index.js
@@ -0,0 +1,72 @@
+'use strict'
+
+const ScriptLinker = require('script-linker')
+const { builtinModules } = require('module')
+const path = require('path')
+
+module.exports = class Boot {
+  constructor (drive, opts = {}) {
+    this.drive = drive
+    this.modules = new Set(builtinModules)
+
+    this.linker = new ScriptLinker({
+      cacheSize: Infinity,
+      readFile: async (name) => {
+        const buffer = await this.drive.get(name)
+        if (!buffer) throw new Error('No entry: ' + name)
+        return buffer
+      }
+    })
+
+    if (opts.modules) for (const name of opts.modules) this.modules.add(name)
+  }
+
+  async start (entrypoint) {
+    if (!entrypoint) {
+      const pkg = await this.drive.get('/package.json')
+      entrypoint = JSON.parse(pkg || '{}').main
+      if (!entrypoint) throw new Error('No entrypoint')
+      entrypoint = path.resolve('/', entrypoint)
+    }
+
+    let first = null
+
+    for await (const dep of this.linker.dependencies(entrypoint)) {
+      if (!first) first = dep
+    }
+
+    const cache = {}
+    const nodeRequire = require
+    const { linker, modules } = this
+
+    return run(first.module)
+
+    function run (mod) {
+      if (cache[mod.filename]) return cache[mod.filename]
+
+      const m = cache[mod.filename] = {
+        exports: {}
+      }
+
+      require.cache = cache
+
+      const wrap = new Function('require', '__dirname', '__filename', 'module', 'exports', mod.source) // eslint-disable-line no-new-func
+      wrap(require, mod.dirname, mod.filename, m, m.exports)
+
+      return m
+
+      function require (req) {
+        if (modules.has(req)) {
+          return nodeRequire(req)
+        }
+
+        for (const r of mod.resolutions) {
+          if (r.input === req) {
+            if (!r.output) throw new Error('MODULE_NOT_FOUND: ' + r.input)
+            return run(linker.modules.get(r.output)).exports
+          }
+        }
+      }
+    }
+  }
+}
