470a5a5adb42ee10e6006f772526ce0bca25690b
Lucas Barrena <lucasbarrena98@gmail.com>
2023-03-18 10:52:09 -0300

    remove test edit

--- test.js
+++ test.js
@@ -130,9 +130,6 @@ test('require file within drive', async function (t) {
   await boot.warmup()
 
   t.is(boot.start(), 'hello func: 4')
-
-  const source = boot.stringify()
-  t.is(eval(source), 'hello func: 4') // eslint-disable-line no-eval
 })
 
 test('require module with prebuilds', async function (t) {
