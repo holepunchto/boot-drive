9f437d9cde4c8bfae55c066383279e1964d64dd3
David Mark Clements <david.mark.clements@gmail.com>
2023-10-18 21:05:37 +0200

    fix

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ module.exports = class Boot {
     if (mod.builtin) return
     const dir = mod.linker.drive.readdir(mod.dirname + '/prebuilds')[Symbol.asyncIterator]()
     const hasBuilds = (await dir.next()).done === false
-    dir.destroy()
+    dir.return()
     if (!hasBuilds) return
     const runtime = this._isNode ? 'node' : 'bare'
     const pkg = await mod.loadPackage()
