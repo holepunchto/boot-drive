48bb82c9816693c7b20a533dc7341fa25456103f
David Mark Clements <xdxmxc@gmail.com>
2023-10-19 16:18:43 +0200

    Bare support (#55)
    * bare-support
    
    * builtins do not have builtins
    
    * fix
    
    * fix
    
    * fix
    
    * Update index.js
    
    * Update index.js

--- index.js
+++ index.js
@@ -31,12 +31,13 @@ module.exports = class Boot {
   }
 
   async _savePrebuildToDisk (mod) {
-    const hasBuilds = resolve(mod, 'node-gyp-build')
+    if (mod.builtin) return
+    const dir = mod.linker.drive.readdir(mod.dirname + '/prebuilds')[Symbol.asyncIterator]()
+    const hasBuilds = (await dir.next()).done === false
+    await dir.return()
     if (!hasBuilds) return
-
     const runtime = this._isNode ? 'node' : 'bare'
     const pkg = await mod.loadPackage()
-
     let prebuild = await this._getLocalPrebuild(pkg, runtime)
     if (!prebuild && runtime === 'bare') prebuild = await this._getLocalPrebuild(pkg, 'node')
 
@@ -108,7 +109,6 @@ module.exports = class Boot {
     entrypoint = entrypoint ? unixResolve('/', entrypoint) : this.entrypoint
 
     if (this.forceWarmup === false && this.dependencies.has(entrypoint)) return
-
     for await (const dep of this.linker.dependencies(entrypoint, {}, new Set(), this.dependencies)) {
       await this._savePrebuildToDisk(dep.module)
     }
@@ -233,7 +233,7 @@ function createRequire (run, ctx, mod) {
     const r = mod.requires[req]
 
     if (r.isBuiltin) {
-      return ctx.builtinRequire(r.output)
+      return ctx.builtinRequire(process.versions.bare && req === 'addon' ? req : r.output)
     }
 
     if (!r.output) throw new Error('Could not resolve ' + req + ' from ' + mod.dirname)
@@ -241,16 +241,6 @@ function createRequire (run, ctx, mod) {
     const dep = ctx.dependencies[r.output]
     return run(run, ctx, dep)
   }
-}
-
-function resolve (mod, input) {
-  for (const r of mod.resolutions) {
-    if (r.input === input) {
-      if (!r.output) break
-      return r.output
-    }
-  }
-  return null
 }
 
 async function atomicWriteFile (filename, buffer) {
